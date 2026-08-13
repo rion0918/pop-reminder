661291ea6646985c7fa9ce6b6624c5ba6f6afc45
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2026-08-13 13:23:20 +0900

    コミットの修正

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  forceExit: true,
   testMatch: ['<rootDir>/src/**/*.ui.spec.tsx'],
   transformIgnorePatterns: [
     'node_modules/(?!(.pnpm|(jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|nativewind|react-native-css-interop|react-native-reanimated|react-native-worklets))',
